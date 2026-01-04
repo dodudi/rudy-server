dfedbf21744a4636ba96085169e0f2976a32c636
Rudy <idejrdud@gmail.com>
2026-01-04 14:53:33 +0900

    feat: TimestampRequest 에 간단한 주석 추가

--- src/main/java/kr/it/rudy/server/common/dto/TimestampRequest.java
+++ src/main/java/kr/it/rudy/server/common/dto/TimestampRequest.java
@@ -1,5 +1,11 @@
 package kr.it.rudy.server.common.dto;
 
+/**
+ * @param input          타임스탬프 혹은 날짜 문자열 데이터
+ * @param conversionType 현재시간, 타임스탬프, 날짜 타입
+ * @param timezone       타임존
+ *
+ */
 public record TimestampRequest(
         String input,
         String conversionType,
